a33cd228ff6cd7199a11cbdc733c89c4e6ad5301
jordonkane <kanejord99@outlook.com>
2025-01-24 18:59:02 -0500

    [Test] Configured UtilsTests

--- ChromaSharp.Tests/UtilsTests.cs
+++ ChromaSharp.Tests/UtilsTests.cs
@@ -1,37 +1,28 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 
 namespace ChromaSharp.Tests
 {
+    [TestFixture] // Optional but recommended
     public class UtilsTests
     {
         [Test]
         public void Clamp_WithinRange_ReturnsSameValue()
         {
-            // Arrange
             int value = 50;
             int min = 0;
             int max = 100;
-
-            // Act
             int result = ChromaSharp.Utils.Utils.Clamp(value, min, max);
-
-            // Assert
-            Assert.AreEqual(50, result);
+            Assert.That(result, Is.EqualTo(50));
         }
 
         [Test]
         public void Clamp_BelowRange_ReturnsMin()
         {
-            // Arrange
             int value = -10;
             int min = 0;
             int max = 100;
-
-            // Act
             int result = ChromaSharp.Utils.Utils.Clamp(value, min, max);
-
-            // Assert
-            Assert.AreEqual(0, result);
+            Assert.That(result, Is.EqualTo(0));
         }
     }
 }
