5f8f998879afd61d84697bca02824768d9627eb5
jordonkane <kanejord99@outlook.com>
2025-01-24 14:02:46 -0500

    Implement YCbCr methods

--- Converters/YCbCr.cs
+++ Converters/YCbCr.cs
@@ -15,10 +15,20 @@ namespace ChromaSharp.ColorSpaces
 
         public Rgb ToRgb()
         {
+            int r = (int)(Y + 1.402 * (Cr - 128));
+            int g = (int)(Y - 0.344136 * (Cb - 128) - 0.714136 * (Cr - 128));
+            int b = (int)(Y + 1.772 * (Cb - 128));
+            return new Rgb(
+                Math.Max(0, Math.Min(255, r)),
+                Math.Max(0, Math.Min(255, g)),
+                Math.Max(0, Math.Min(255, b))
+            );
         }
 
         public Cmyk ToCmyk()
         {
+            Rgb rgb = ToRgb();
+            return rgb.ToCmyk();
         }
 
         public Hsv ToHsv()
